1762cbf7a09cfbfd2578040d49f8a5082ae3845c
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-26 21:01:24 +0300

    dbcontext in program.cs + gitignored appsettings.development

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
