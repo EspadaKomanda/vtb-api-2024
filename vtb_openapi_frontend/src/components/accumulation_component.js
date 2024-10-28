c44183482213790ad92f76c0ef1054d06ea3263d
Rudolf31 <rudolfshevchenko@gmail.com>
2024-10-28 22:12:58 +0300

    was added zustand and was maked first step with new component

--- vtb_openapi_frontend/src/components/accumulation_component.js
+++ vtb_openapi_frontend/src/components/accumulation_component.js
@@ -1,10 +1,15 @@
-
+"use client";
+import { useState } from "react"
+import useAccumulationStore from "../stores/accumulation_store.js"
 
 export default function AccumulationComponent() {
+
+    const { tourName, setTourName, sum, setSum, actualSum, setActualSum } = useAccumulationStore();
+
     return (
         <div className="bg-custom-blur min-h-96">
-            <div className="conteiner">
-
+            <div className="conteiner py-24">
+                  <h3 className="text-white text-5xl font-bold">На тур в {tourName} вместе с ВТБ</h3>          
             </div>
         </div>
     )
--- vtb_openapi_frontend/src/stores/accumulation_store.js
+++ vtb_openapi_frontend/src/stores/accumulation_store.js
@@ -1,1 +1,13 @@
 
+import { create } from 'zustand'
+
+const useAccumulationStore = create((set) => ({
+    tourName :  "Финал ВТБ 2024",
+    setTourName: (name) => set({tourName: name}),
+    sum: 0,
+    setSum: (sum) => set({sum: sum}),
+    actualSum: 0,
+    setActualSum: (sum) => set({actualSum: sum})
+}))
+
+export default useAccumulationStore
