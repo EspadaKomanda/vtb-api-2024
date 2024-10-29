da7a0d0ddec424073173c8750ccdcc8acd4c184f
ereshkigal <roman.khlynin@bk.ru>
2024-10-29 16:06:14 +0300

    adding repository

---
+++ TourService/Repository/FindOptions.cs
@@ -0,0 +1,8 @@
+namespace TourService.Repository
+{
+    public class FindOptions
+    {
+        public bool IsIgnoreAutoIncludes { get; set; }
+        public bool IsAsNoTracking { get; set; }
+    }
+}
---
+++ TourService/Repository/Repository.cs
@@ -0,0 +1,79 @@
+using System.Linq.Expressions;
+using TourService.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace TourService.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationContext _empDBContext;
+        public Repository(ApplicationContext empDBContext)
+        {
+            _empDBContext = empDBContext;
+        }
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            await _empDBContext.Set<TEntity>().AddAsync(entity);
+            return await _empDBContext.SaveChangesAsync()>= 0;
+        }
+        public async Task<bool> AddManyAsync(IEnumerable<TEntity> entities)
+        {
+            await _empDBContext.Set<TEntity>().AddRangeAsync(entities);
+            return await _empDBContext.SaveChangesAsync()>= 0;
+        }
+        public bool Delete(TEntity entity)
+        {
+            _empDBContext.Set<TEntity>().Remove(entity);
+            return _empDBContext.SaveChanges()>= 0;
+        }
+        public bool DeleteMany(Expression<Func<TEntity, bool>> predicate)
+        {
+            var entities = Find(predicate);
+            _empDBContext.Set<TEntity>().RemoveRange(entities);
+            return _empDBContext.SaveChanges()>= 0;
+        }
+        public async Task<TEntity> FindOneAsync(Expression<Func<TEntity, bool>> predicate, FindOptions? findOptions = null)
+        {
+            return await Get(findOptions).FirstOrDefaultAsync(predicate)! ?? throw new NullReferenceException("Entity not found!");
+        }
+        public IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate, FindOptions? findOptions = null)
+        {
+            return Get(findOptions).Where(predicate);
+        }
+        public IQueryable<TEntity> GetAll(FindOptions? findOptions = null)
+        {
+            return Get(findOptions);
+        }
+        public bool Update(TEntity entity)
+        {
+            _empDBContext.Set<TEntity>().Update(entity);
+            return _empDBContext.SaveChanges()>= 0;
+        }
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _empDBContext.Set<TEntity>().AnyAsync(predicate);
+        }
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _empDBContext.Set<TEntity>().CountAsync(predicate);
+        }
+        private DbSet<TEntity> Get(FindOptions? findOptions = null)
+        {
+            findOptions ??= new FindOptions();
+            var entity = _empDBContext.Set<TEntity>();
+            if (findOptions.IsAsNoTracking && findOptions.IsIgnoreAutoIncludes)
+            {
+                entity.IgnoreAutoIncludes().AsNoTracking();
+            }
+            else if (findOptions.IsIgnoreAutoIncludes)
+            {
+                entity.IgnoreAutoIncludes();
+            }
+            else if (findOptions.IsAsNoTracking)
+            {
+                entity.AsNoTracking();
+            }
+            return entity;
+        }
+    }
+}
