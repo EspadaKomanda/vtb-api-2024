6b33829b6ece3bbcd606d7d7bdb3aa1934c9a17a
Nikolai Papin <nikolai@weirdcat.ru>
2024-11-01 20:13:03 +0300

    Implemented transaction methods

--- UserService/Repositories/Repository.cs
+++ UserService/Repositories/Repository.cs
@@ -1,12 +1,14 @@
 using System.Linq.Expressions;
 using UserService.Database;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace UserService.Repository;
 
 public class Repository<TEntity>(ApplicationContext empDBContext) : IRepository<TEntity> where TEntity : class
 {
     private readonly ApplicationContext _empDBContext = empDBContext;
+    private IDbContextTransaction? _transaction;
 
     public async Task<bool> AddAsync(TEntity entity)
     {
@@ -71,5 +73,30 @@ public class Repository<TEntity>(ApplicationContext empDBContext) : IRepository<
             entity.AsNoTracking();
         }
         return entity;
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        _transaction = await _empDBContext.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+        if (_transaction != null)
+        {
+            await _transaction.CommitAsync();
+            _transaction.Dispose();
+            _transaction = null;
+        }
+    }
+
+    public async Task RollbackTransactionAsync()
+    {
+        if (_transaction != null)
+        {
+            await _transaction.RollbackAsync();
+            _transaction.Dispose();
+            _transaction = null;
+        }
     }
 }
