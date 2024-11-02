1e34cd4fba81d30dd345ecc1fc1ad9d24e15fc74
Nikolai Papin <nikolai@weirdcat.ru>
2024-11-02 22:58:09 +0300

    Deprecated dispose method in UOW

--- UserService/Repositories/UnitOfWork.cs
+++ UserService/Repositories/UnitOfWork.cs
@@ -6,6 +6,7 @@ namespace UserService.Repositories;
 
 public class UnitOfWork : IUnitOfWork
 {
+    private readonly ILogger<UnitOfWork> _logger;
     private readonly ApplicationContext _context;
     public IRepository<User> Users { get; }
     public IRepository<Role> Roles { get; }
@@ -15,10 +16,11 @@ public class UnitOfWork : IUnitOfWork
     public IRepository<ResetCode> ResetCodes { get; }
     public IRepository<PersonalData> PersonalDatas { get; }
 
-    public UnitOfWork(ApplicationContext context, IRepository<User> users, IRepository<Role> roles, IRepository<Meta> metas,
+    public UnitOfWork(ILogger<UnitOfWork> logger, ApplicationContext context, IRepository<User> users, IRepository<Role> roles, IRepository<Meta> metas,
         IRepository<VisitedTour> visitedTours, IRepository<RegistrationCode> registrationCodes, IRepository<ResetCode> resetCodes,
         IRepository<PersonalData> personalDatas)
     {
+        _logger = logger;
         _context = context;
         Users = users;
         Roles = roles;
@@ -34,8 +36,10 @@ public class UnitOfWork : IUnitOfWork
         return _context.SaveChanges();
     }
 
+    [Obsolete("UnitOfWork is not supposed to be manually disposed.")]
     public void Dispose()
     {
+        _logger.LogWarning("UnitOfWork is not supposed to be manually disposed. Causing this method may cause trouble!");
         _context.Dispose();
         GC.SuppressFinalize(this);
     }
