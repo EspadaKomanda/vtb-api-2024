d05ffb7bb98da3c69e1520d370843607857a148c
Nikolai Papin <nikolai@weirdcat.ru>
2024-11-04 15:52:51 +0300

    Interface

--- MailService/Services/Mail/IMailService.cs
+++ MailService/Services/Mail/IMailService.cs
@@ -1,1 +1,9 @@
+using MailService.Models.Mail.Requests;
+using MailService.Models.Mail.Responses;
+
 namespace MailService.Services.Mail;
+
+public interface IMailService
+{
+    Task<SendMailResponse> SendMailAsync(SendMailRequest request);
+}
