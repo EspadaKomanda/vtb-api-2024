400e32dbad06f20d96e5bc6cb9527c0b0c765eb3
ereshkigal <roman.khlynin@bk.ru>
2024-11-05 15:19:21 +0300

    Removing unnessecary folder

--- UserService/Exceptions/Account/CodeHasNotExpiredException.cs
+++
@@ -1,9 +0,0 @@
-namespace UserService.Exceptions.Account;
-
-[Serializable]
-public class CodeHasNotExpiredException : Exception
-{
-    public CodeHasNotExpiredException() { }
-    public CodeHasNotExpiredException(string message) : base(message) { }
-    public CodeHasNotExpiredException(string message, Exception inner) : base(message, inner) { }
-}
--- UserService/Exceptions/Account/UserExistsError.cs
+++
@@ -1,9 +0,0 @@
-namespace UserService.Exceptions.Account;
-
-[Serializable]
-public class UserExistsException : Exception
-{
-    public UserExistsException() { }
-    public UserExistsException(string message) : base(message) { }
-    public UserExistsException(string message, Exception inner) : base(message, inner) { }
-}
