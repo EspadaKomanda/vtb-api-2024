35b2639965f017e77e8c7364aa5783d982c1205c
Nikolai Papin <nikolai@weirdcat.ru>
2024-11-09 22:55:40 +0300

    Made ValidatePromocodeApplicationRequest inherited, implemented validation method

--- PromoService/Models/PromoApplication/Requests/ValidatePromocodeApplicationRequest.cs
+++ PromoService/Models/PromoApplication/Requests/ValidatePromocodeApplicationRequest.cs
@@ -2,11 +2,4 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PromoService.Models.PromoApplication.Requests;
 
-public class ValidatePromocodeApplicationRequest
-{
-    [Required]
-    public string PromoCode { get; set; } = null!;
-
-    public List<long>? TourIds { get; set; }
-    public List<long>? EntertainmentIds { get; set; }
-}
+public class ValidatePromocodeApplicationRequest : RegisterPromoUseRequest;
--- PromoService/Services/PromoApplication/PromoApplicationService.cs
+++ PromoService/Services/PromoApplication/PromoApplicationService.cs
@@ -1,4 +1,5 @@
 using PromoService.Database.Models;
+using PromoService.Exceptions.PromoApplication;
 using PromoService.Models.PromoApplication;
 using PromoService.Models.PromoApplication.Requests;
 using PromoService.Models.PromoApplication.Responses;
@@ -50,30 +51,95 @@ public class PromoApplicationService(IUnitOfWork unitOfWork, IUsersService users
 
     }
 
-    public Task<RegisterPromoUseResponse> RegisterPromoUse(long userId, RegisterPromoUseRequest request)
+    public async Task<RegisterPromoUseResponse> RegisterPromoUse(long userId, RegisterPromoUseRequest request)
     {
-        throw new NotImplementedException();
+        try {
+                
+            if ((await ValidatePromocodeApplication(userId, (ValidatePromocodeApplicationRequest)request)).IsSuccess)
+            {
+                // TODO: Register promo use
+                throw new NotImplementedException();
+            }
+        }
+        catch
+        {
+            throw;
+        }
+        return new();
     }
 
     public async Task<ValidatePromocodeApplicationResponse> ValidatePromocodeApplication(long userId, ValidatePromocodeApplicationRequest request)
     {
         User user;
+        Promo promo;
 
         try
         {
             _logger.LogDebug("Finding user with id {UserId}", userId);
             user = await _usersService.GetUser(userId);
-
-            // TODO: make checks
-            return new ValidatePromocodeApplicationResponse
-            {
-                IsSuccess = true
-            };
         }
         catch
         {
             _logger.LogError("Failed to find user with id {UserId}", userId);
             throw;
         }
+
+        try
+        {
+            _logger.LogDebug("Finding promo with code {PromoId}", request.PromoCode);
+            promo = await _uow.PromoRepo.FindOneAsync(p => p.Code == request.PromoCode);
+        }
+        catch
+        {
+            _logger.LogError("Failed to find promo with code {PromoId}", request.PromoCode);
+            throw;
+        }
+
+        // Is deleted check
+        _logger.LogDebug("Checking if promo is deleted");
+        if (promo.Deleted)
+        {
+            _logger.LogDebug("Promocode is deleted");
+            throw new PromocodeInapplicapleException("Promocode is deleted");
+        }
+
+        // Account age check
+        _logger.LogDebug("Checking account age");
+        var accountAge = (DateTime.Now - user.CreationDate).Days / 365;
+        if (promo.MinAccountAge != null && accountAge < promo.MinAccountAge || promo.MaxAccountAge != null && accountAge > promo.MaxAccountAge)
+        {
+            _logger.LogDebug("Account age is out of range");
+            throw new PromocodeInapplicapleException("Account age is out of range");
+        }
+
+        // Max per user check
+        _logger.LogDebug("Checking max per user");
+        var userPromos = _uow.UserPromoRepo.Find(p => p.UserId == userId);
+        if (promo.MaxPerUser != null && userPromos.Count() >= promo.MaxPerUser)
+        {
+            _logger.LogDebug("Limit on uses of this promocode has been reached by the user");
+            throw new PromocodeInapplicapleException("Limit on uses of this promocode has been reached by the user");
+        }
+
+        // Max per everyone check
+        _logger.LogDebug("Checking max per everyone");
+        if (promo.MaxPerEveryone != null && _uow.UserPromoRepo.Find(p => p.PromoId == promo.Id).Count() >= promo.MaxPerEveryone)
+        {
+            _logger.LogDebug("Limit on uses of this promocode has been reached by everyone");
+            throw new PromocodeInapplicapleException("Limit on uses of this promocode has been reached by everyone");
+        }
+
+        // Active date check
+        _logger.LogDebug("Checking active date");
+        if (promo.StartDate != null && DateTime.Now < promo.StartDate || promo.EndDate != null && DateTime.Now > promo.EndDate)
+        {
+            _logger.LogDebug("Promocode is not yet active or already expired");
+            throw new PromocodeInapplicapleException("Promocode is not yet active or already expired");
+        }
+        _logger.LogDebug("Promocode {PromoId} for user {UserId} passed all checks", promo.Id, userId);
+        return new()
+        {
+            IsSuccess = true
+        };
     }
 }
