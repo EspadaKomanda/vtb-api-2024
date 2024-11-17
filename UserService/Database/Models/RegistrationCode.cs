1fd332853ae91879b670cc2c490157d527048603
Nikolai Papin <nikolai@weirdcat.ru>
2024-11-17 23:46:43 +0300

    Fixed wrong length of regcode in db model

--- UserService/Database/Models/RegistrationCode.cs
+++ UserService/Database/Models/RegistrationCode.cs
@@ -13,7 +13,7 @@ public class RegistrationCode
     // FIXME: Match the six-character registration code template
     [Required]
     [Column(TypeName = "VARCHAR(6)")]
-    public string Code { get; set; } = Guid.NewGuid().ToString();
+    public string Code { get; set; } = Guid.NewGuid().ToString()[..6];
 
     public DateTime ExpirationDate { get; set; } = DateTime.Now.AddMinutes(10);
 
